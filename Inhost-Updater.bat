ff4f7f346fc0c7cfe0ac711206851714e4c9fc4c
wawn1782 <hermawan.dony@framas.com>
2026-06-29 17:51:03 +0700

    Auto-update versi PBD terbaru

--- Inhost-Updater.bat
+++ Inhost-Updater.bat
@@ -1,30 +1,38 @@
 @echo off
-color 0A
+color 0B
 title Inhost Auto Updater
 
-echo ==========================================
-echo  Inhost Client Updater
-echo ==========================================
+echo.
+echo  ======================================================
+echo     ___       _               _     
+echo    ^|_ _^|____ ^| ^|__   ___  ___^| ^|_   
+echo     ^| ^|/ _ \^| '_ \ / _ \/ __^| __^|  
+echo     ^| ^|  __/^| ^| ^| ^| (_) \__ \ ^|_   
+echo    ^|___\___^|_^| ^|_^|\___/^|___/\__^|  
+echo.
+echo     [ Client Auto-Updater System ]
+echo  ======================================================
 echo.
 
 set GITHUB_RAW=https://raw.githubusercontent.com/hermawan-dony/inhost-pbd/main
 
-call :DownloadFile bcapp.pbd
+call :DownloadFile "bcapp.pbd"
 echo.
-call :DownloadFile custom.pbd
+call :DownloadFile "custom.pbd"
 echo.
-call :DownloadFile ceisa.pbd
+call :DownloadFile "ceisa.pbd"
 echo.
 
-echo ==========================================
-echo Update Selesai! Semua file siap digunakan.
-echo ==========================================
+echo  ======================================================
+echo   [!] Update Selesai! Semua file siap digunakan.
+echo  ======================================================
+echo.
 pause
 exit /b
 
 :DownloadFile
-set FILENAME=%1
-echo [Cek Update] %FILENAME% ...
+set FILENAME=%~1
+echo  --^> Memeriksa [ %FILENAME% ] ...
 
 rem Mengecek apakah file etag lokal ada, lalu download secara kondisional
 if exist "%FILENAME%.etag" (
@@ -34,6 +42,6 @@ if exist "%FILENAME%.etag" (
 )
 
 if %errorlevel% neq 0 (
-    echo [Gagal] Terjadi kesalahan saat memeriksa/mendownload %FILENAME%.
+    echo      [x] Gagal mendownload %FILENAME%. Silakan cek koneksi internet!
 )
 exit /b
